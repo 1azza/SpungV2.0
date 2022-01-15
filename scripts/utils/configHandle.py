55c7c3773373303880d312a753441487566063af
1azza <larryrennoldson@icloud.com>
2022-01-15 14:24:34 +0000

    Fixed hidden token but fuckd up spung

--- scripts/utils/configHandle.py
+++ scripts/utils/configHandle.py
@@ -6,6 +6,7 @@ import discord
 class ConfigHandler:
     def __init__(self):
         print('Loading config...')
+        self.ERROR = 0
         self.intents = discord.Intents.default()
         self.intents.members = True
         with open(r'config\config.yml') as f:
@@ -13,15 +14,34 @@ class ConfigHandler:
         general = (cfg["General"])
         important = (cfg["Important"])
         visual = (cfg["Visual"])
-        for key, value in important.items():
-            self.ERROR = 'Please enter a valid Token in config\config.yml'
-        print(general)
-        print(important)
         self.TOKEN = important.get('Token')
-        self.PREFIX = general.get('Prefix')
-        self.ADMINS = general.get('Admins')
-        self.PASSWORD = general.get('Passwd')
-        self.ACTIVITY = visual.get('Activity')
+        def set():
+            self.PREFIX = general.get('Prefix')
+            self.ADMINS = general.get('Admins')
+            self.PASSWORD = general.get('Passwd')
+            self.ACTIVITY = visual.get('Activity')
+            print('Found Token!')
+            print(self.TOKEN)
+
+        if self.TOKEN == None:
+            try:
+                with open(r'config\secret.txt') as f:
+                        print('Looking in secret.txt')
+                        self.TOKEN = f.readline()
+                if len(self.TOKEN) > 10:
+                    set()
+                else:
+                    self.ERROR = 'Please enter a valid Token in config\secret.txt'
+
+
+            except:
+                self.ERROR = 'Please enter a valid Token in config\config.yml'
+
+
+        else:
+            set()
+
+
 
     def getBotConfig(self):
         bot = commands.Bot(command_prefix=self.PREFIX, intents=self.intents)
@@ -30,14 +50,17 @@ class ConfigHandler:
         return bot
 
     def configError(self):
-        print('ERROR:')
         if self.ERROR:
-            print(config.ERROR)
-        else:
-            print('unknown error')
+            print(self.ERROR)
+            quit()
+        pass
+
+
+
 
 
 
 
 config = ConfigHandler()
+config.configError()
 bot = config.getBotConfig()
