f7ff2045646aa9d36cc59488f13b7a94aba0143c
1azza <larryrennoldson@icloud.com>
2022-01-15 15:22:08 +0000

    Added slash commands now finnaly working!

--- scripts/cogs/pong.py
+++ scripts/cogs/pong.py
@@ -1,16 +1,26 @@
 from discord.ext import commands
 import discord
 from scripts.utils.configHandle import config, bot
+from discord.commands import slash_command, user_command
+from discord.commands import Option
+import random
 class games(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self._last_member = None
 
-    @commands.command()
+    @slash_command(description="Test the bots speed")
     async def ping(self, ctx):
-            await ctx.reply('Pong!')
-    @commands.command()
+            await ctx.respond('Pong! {0}'.format(bot.latency))
+
+
+
+    @slash_command(description="How big is you PP?")
     async def pp(self, ctx):
-            await ctx.send('small')
+            await ctx.respond('small')
+
+    @slash_command(guild_ids=[861316984360402974], description="Random number from 1-100")
+    async def number(self, ctx):
+            await ctx.respond(random.randint(1, 100))
 def setup(bot):
     bot.add_cog(games(bot))
--- scripts/cogs/spung.py
+++ scripts/cogs/spung.py
@@ -5,6 +5,8 @@ import cv2
 import numpy as np
 import re
 import requests
+from discord.commands import slash_command, user_command
+from discord.commands import Option
 class spung_commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -14,6 +16,7 @@ class spung_commands(commands.Cog):
 
     async def dlAvatar(self, ctx, user):
         user = await bot.fetch_user(user)
+        print('Spunging:', user, '...')
         url = user.display_avatar.url
         img_data = requests.get(url).content
         with open(self.PATH, 'wb') as handler:
@@ -24,24 +27,20 @@ class spung_commands(commands.Cog):
     async def main_spung(self, ctx):
         imgCanny =  cv2.Canny(self.IMG, 100, 100)
         cv2.imwrite(self.PATH, imgCanny)
-        await ctx.send(file=discord.File(self.PATH))
+        await ctx.respond(file=discord.File(self.PATH))
 
-    @commands.command()
+    @slash_command(guild_ids=[861316984360402974], description="Spung another user")
     async def spung(self, ctx, arg):
             user = re.sub("[^0-9]", "", arg)
             await self.dlAvatar(ctx, user)
 
 
-    @commands.command()
-    async def spungbomb(self, ctx):
-        for user in ctx.guild.members:
-            await self.dlAvatar(ctx, user)
 
-    @commands.command()
+    @slash_command(guild_ids=[861316984360402974], description="Spung any image")
     async def spunk(self, ctx):
         def check(message):
             return message.author == ctx.author and bool(message.attachments)
-        await ctx.send('**Waiting for an attachment...**')
+        await ctx.respond('**Waiting for an attachment...**')
         resp = await bot.wait_for('message', check=check)
         image = resp.attachments[0]
         await image.save(fp=self.PATH)
